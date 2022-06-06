28d7f81c693351d75747714c1ca3fa6e3da28fe6
Khanh <jbkhanhtran@gmail.com>
2022-06-06 17:17:43 +0700

    add SharedPref

--- app/src/main/java/com/example/myapplication/Activities/HomeActivity.java
+++ app/src/main/java/com/example/myapplication/Activities/HomeActivity.java
@@ -1,16 +1,31 @@
 package com.example.myapplication.Activities;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.FragmentManager;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.TextView;
 
 import com.example.myapplication.R;
+import com.google.android.material.navigation.NavigationView;
 
 public class HomeActivity extends AppCompatActivity {
 
+    int maquyen = 0;
+    SharedPreferences sharedPreferences;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        //lấy file share prefer
+        sharedPreferences = getSharedPreferences("luuquyen", Context.MODE_PRIVATE);
+        maquyen = sharedPreferences.getInt("maquyen",0);
+
     }
 }
